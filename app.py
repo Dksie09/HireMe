570c6cb49509d1a96376c950d18e5ca3e5c5220d
Dakshi <dakshiegoel@gmail.com>
2023-04-24 19:51:00 +0530

    runner

---
+++ app.py
@@ -0,0 +1,9 @@
+#run the 1_🔍_Home.py file
+
+import subprocess
+
+# Specify the path to the Python file you want to run
+file_path = './1_🔍_Home.py'
+
+# Run the Streamlit app
+subprocess.run(['streamlit', 'run', file_path])
